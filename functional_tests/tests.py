522977083366b624966946d3559da5c4a51f3940
Vinícius <vinybf@hotmail.com>
2021-02-28 19:34:42 -0300

    Integrando o Bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 MAX_WAIT = 5
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
