cc2e32901688a42f412516582f14ae8b9d3d6803
Vinícius <vinybf@hotmail.com>
2021-02-28 19:32:38 -0300

    Bootstrap + base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -63,6 +63,31 @@ class NewVisitorTest(LiveServerTestCase):
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
+
+        def test_layout_and_styling(self):
+        # Edith entra na home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela nota que o input box está centralizado
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e nota que o input
+        # também está centralizado
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
     
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Maria começa uma nova lista
